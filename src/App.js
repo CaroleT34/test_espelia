abe4f95334ae122700a0e5025ab11f7558a2f3b6
Carole TOULORGE <carole.toulorge@hotmail.fr>
2022-07-27 12:42:04 +0200

    données dans le tableau

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [buildings, setBuildings] = useState(null)
+  const [page, setPage] = useState(1)
+  useEffect(() => {
+    fetch("https://test-api.espedata.fr/index.php/api/buildings?page="+page)
+    .then(response => response.json())
+    .then(data => setBuildings(data))
+  }, [page]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <thead>
+            <tr>
+              <th>id</th>
+              <th>picture</th>
+              <th>buildingName</th>
+              <th>streetNumber</th>
+              <th>streetName</th>
+              <th>postcode</th>
+              <th>city</th>
+              <th>country</th>
+              <th>latitude</th>
+              <th>longitude</th>
+              <th>constructionDate</th>
+              <th>commentary</th>
+            </tr>
+        </thead>
+        <tbody>
+          {buildings && buildings["hydra:member"].map(building => (
+            <tr key={building.id}>
+              <td className="paddingH25">{building.id}</td>
+              <td>
+                <img className="picture" src={building.picture} alt="image du bâtiment"/>
+              </td>
+              <td>{building.buildingName}</td>
+              <td>{building.streetNumber}</td>
+              <td>{building.streetName}</td>
+              <td>{building.postcode}</td>
+              <td>{building.city}</td>
+              <td>{building.country}</td>
+              <td>{building.latitude}</td>
+              <td>{building.longitude}</td>
+              <td>{building.constructionDate}</td>
+              <td>{building.commentary}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {buildings && buildings["hydra:view"]["@id"] !== buildings["hydra:view"]["hydra:first"] && <button onClick={() => setPage(page-1)}> Page précédente </button>}
+      {buildings && buildings["hydra:view"]["@id"] !== buildings["hydra:view"]["hydra:last"] && <button onClick={() => setPage(page+1)}> Page suivante </button>}
+    
     </div>
   );
 }
